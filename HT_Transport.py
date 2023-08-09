f4489a4b303467ce75475d571207a59fd46d07f8
Dronier Rémi <95101715+M89-dev@users.noreply.github.com>
2023-08-09 17:48:06 +0200

    Update HT_Transport.py

--- HT_Transport.py
+++ HT_Transport.py
@@ -1,5 +1,4 @@
 import xml.etree.ElementTree as ET
-import random
 
 class Calcul_HT_Transport:
 
@@ -21,180 +20,100 @@ class Calcul_HT_Transport:
         self.object_tarifs = self.root_tarif.findall(".//ObjectTarif")
         self.object_condition_taxations = self.root_taxation.findall(".//ObjectConditionTaxation")
 
-    # Fonction pour prendre au hasard un Client et un Expediteur différents
-    def get_info_client(self):
-        random_expediteur = random.choice(self.object_clients)
-        random_destinataire = random.choice(self.object_clients)
+        self.montant_tarif = None
+        self.montant_taxation = None
 
-        while random_destinataire == random_expediteur:
-            random_destinataire = random.choice(self.object_clients)
+    # Fonction qui permet de récuperer les infos pour l'expediteur, le destinataire et autre ....
+    def get_user_info(self):
+        self.client_id_expediteur = str(input("Entrez l'id du client expéditeur : "))
+        self.client_id_destinataire = str(input("Entrez l'id du client destinataire : "))
+        self.nombre_colis = int(input("Entrez le nombre de colis : "))
+        self.poids_expedition = float(input("Entrez le poids de l'expédition : "))
+        self.port_paye = input("Le transport est payé par le destinataire (O/N)? ").upper()
+        self.boolean_port_paye()
 
-        return random_expediteur, random_destinataire
+    # Fonction qui transforme la réponse pour savoir qui paye en boolean
+    def boolean_port_paye(self):
+        self.port_paye = self.port_paye == "O"
 
-    # Fonction qui sert à saisir le nombre de colis et le poids de l’expédition.
-    def input_info(self):
-        print("Quelle est le nombre total de colis ?")
-        nombre_colis = int(input("> "))
-        
-        print("Quel est le poids total des colis ? ")
-        poids_colis = float(input("> "))
+    # Fonction qui permet de récuperer toutes les informations nécessaire sur le destinataire 
+    def get_info_destinataire(self):
+        ville_destinataire = None
 
-
-        return nombre_colis, poids_colis
-
-    # Fonction pour prendre au hasard de l’expéditeur ou du destinataire qui règle le transport
-    def get_payement_info(self, *args):
-        return random.choice(args)
-
-    # Fonction qui sert à derterminer ou se trouve la zone du destinataire
-    def get_zone_destinataire(self, client_destinataire):
-        ville_dest = client_destinataire.find("ville").text
+        for client in self.object_clients:
+            if client.find("idClient").text == self.client_id_destinataire:
+                ville_destinataire = client.find("ville").text
 
         for localite in self.object_localites:
-            ville_loc = localite.find("ville").text
-            zone_loc = localite.find("zone").text
+            if localite.find("ville").text == ville_destinataire:
+                return localite.find("zone").text, localite.find("codePostal").text
 
-            if ville_loc == ville_dest:
-                return zone_loc
-
-    # Fonction qui permet de recuperer tous les tarifs avec le meme ID et code_postale
-    def get_all_zone_id(self, zone_num_id, code_postal_destinataire):
-        liste_zone_tarifs = []
-
+    # Fonction qui permet de récuperer les tarifs de la zone que l'on souhaite         
+    def get_tarif_zone(self, zone, client_id, code_postal):
         for tarif in self.object_tarifs:
-            code_client_id = tarif.find("idClient").text
-            tarif_codeDepartement = tarif.find("codeDepartement").text
-
-            if code_client_id == zone_num_id and code_postal_destinataire == tarif_codeDepartement:
-                liste_zone_tarifs.append(tarif)
-
-        return liste_zone_tarifs
     
-    # Fonction qui permet de determiner les tarifs de la zone en fonction de la zone et de l'ID Client
-    def get_tarif_zone(self, liste_zone_tarifs, zone_destinataire, code_postal_destinataire):
-        for tarif in liste_zone_tarifs:
-            tarif_zone = tarif.find("zone").text
-            tarif_montant = tarif.find("montant").text
-            tarif_id = tarif.find("idClient").text
-            tarif_heritage = tarif.find("idClientHeritage").text
-
-            if tarif_zone == zone_destinataire:
-                if tarif_heritage: # Regarde si le paramètre heritage Client est true
-                    print("Votre département ne possède aucun tarif, C'est donc les tarifs généraux qui sont pris en compte\n")
-                    liste_zone_tarifs = self.get_all_zone_id(tarif_heritage, code_postal_destinataire)
-                    return self.get_tarif_zone(liste_zone_tarifs, zone_destinataire, code_postal_destinataire)
+            if tarif.find('idClient').text == client_id and tarif.find('zone').text == zone and code_postal == tarif.find('codeDepartement').text:
+                if tarif.find('idClientHeritage').text is None:
+                    self.montant_tarif = float(tarif.find('montant').text)
+                    return 
                 else:
-                    return tarif_montant, tarif_id
+                    self.get_tarif_zone(zone, tarif.find('idClientHeritage').text, code_postal)
 
-        # Si un tarif n’existe pas dans la zone déterminée, alors on utilise le tarif de la zone z-1.
-        tarif_montant_inverse, tarif_id_inverse = self.inverse_tarrif_zone(liste_zone_tarifs, zone_destinataire)
-        return tarif_montant_inverse, tarif_id_inverse
+        return None
     
-    # Fonction qui récupère l'inverse de la zone que l'on cherche pour le département
-    def inverse_tarrif_zone(self, liste_zone_tarifs, zone):
-        for tarif in liste_zone_tarifs:
-            tarif_zone = tarif.find("zone").text
-            tarif_monatant = tarif.find("montant").text
-            tarif_id = tarif.find("idClient").text
-
-            if tarif_zone != zone:
-                return tarif_monatant, tarif_id
-
-    # Fonction qui permet de recuperer les taxations en fonction de l'ID Client  
-    def get_taxation_zone(self, id_destinataire):
+    # Fonction qui permet de récuperer les taxations de la zone que l'on souhaite    
+    def get_taxation_zone(self, client_id):
         for condition_taxation in self.object_condition_taxations:
-
-            id_taxation_zone = condition_taxation.find("idClient").text
-            taxePortDu = condition_taxation.find("taxePortDu").text
-            taxePortPaye = condition_taxation.find("taxePortPaye").text
-            useTaxePortDuGenerale = condition_taxation.find("useTaxePortDuGenerale").text
-            useTaxePortPayeGenerale = condition_taxation.find("useTaxePortPayeGenerale").text
-
-            taxePortDuGeneral = self.object_condition_taxations[0].find("taxePortDu").text
-            taxePortPayeGeneral = self.object_condition_taxations[0].find("taxePortPaye").text
-
-            if id_taxation_zone == id_destinataire:
-
-                if useTaxePortDuGenerale and useTaxePortPayeGenerale:
-                    return self.get_payement_info(taxePortDuGeneral, taxePortPayeGeneral)
-                
-                elif useTaxePortPayeGenerale:
-                    return taxePortPayeGeneral
-                
-                elif useTaxePortDuGenerale:
-                    return taxePortDuGeneral
+            if condition_taxation.find("idClient").text == client_id or client_id == "0":
+    
+                if self.port_paye:
+                    if condition_taxation.find("useTaxePortPayeGenerale").text == "true":
+                        self.get_taxation_zone("0")
+                    else:
+                        self.montant_taxation = float(condition_taxation.find("taxePortPaye").text)
+                        return
                 
                 else:
-                    return self.get_payement_info(taxePortDu, taxePortPaye)
-        
-        print("Votre département ne possède aucune taxation, C'est donc les taxations générales qui sont prises en compte\n")
-        return self.get_payement_info(taxePortDuGeneral, taxePortPayeGeneral)
+                    if condition_taxation.find("useTaxePortDuGenerale").text == "true":
+                        self.get_taxation_zone("0")
+                    else:
+                        self.montant_taxation = float(condition_taxation.find("taxePortDu").text)
+                        return
 
-    # Fonction qui permet de centraliser tous les tarifs et taxations de l'expedition
-    def get_tarif_info(self, zone_destinataire, code_postal_destinataire, code_id):
-        liste_zone_tarifs = []
+        self.get_taxation_zone("0")
 
-        liste_zone_tarifs = self.get_all_zone_id(code_id, code_postal_destinataire)
+    # Fonction qui regroupe toutes les infos sur les tarifs et taxations
+    def get_tarif_info(self, zone, client_id, code_postal):
 
-        if len(liste_zone_tarifs) >= 1: # Le client possèdent des tarifs pour ce département
-            tarif_montant, tarif_id = self.get_tarif_zone(liste_zone_tarifs, zone_destinataire, code_postal_destinataire)
-            taxation_montant = self.get_taxation_zone(tarif_id)
-            
-        else: # Le client ne possèdent pas de tarifs pour ce département
-            liste_zone_tarifs = self.get_all_zone_id("0", code_postal_destinataire)
-            tarif_montant, tarif_id = self.get_tarif_zone(liste_zone_tarifs, zone_destinataire, code_postal_destinataire)
-            taxation_montant = self.get_taxation_zone(tarif_id)
+        self.get_taxation_zone(client_id)
+        self.get_tarif_zone(zone, client_id, code_postal)
 
-        return tarif_montant, taxation_montant
+        if self.montant_tarif is None:
+            self.get_tarif_zone(str((int(zone)) - 1), client_id, code_postal)
 
+            if self.montant_tarif is None:
+                self.get_tarif_zone(zone, "0", code_postal)
+
+    # Principale fonction du programme
     def main(self):
-        client_expediteur, client_destinataire = self.get_info_client()
+        self.get_user_info()
+        zone_destinataire, code_postale_destinataire = self.get_info_destinataire()
+        self.get_tarif_info(zone_destinataire, self.client_id_destinataire, code_postale_destinataire)
 
-        # Informations sur le destinataire
-        zone_destinataire = self.get_zone_destinataire(client_destinataire)
-        id_destinataire = client_destinataire.find("idClient").text
-        code_postal_destinataire = client_destinataire.find("codePostal").text
-        raisonSociale_destinataire = client_destinataire.find("raisonSociale").text
-        ville_destinataire = client_destinataire.find("ville").text
-
-        # Informations sur l'expediteur
-        id_expediteur = client_expediteur.find("idClient").text
-        code_postal_expediteur = client_expediteur.find("codePostal").text
-        raisonSociale_expediteur = client_expediteur.find("raisonSociale").text
-        ville_expediteur = client_expediteur.find("ville").text
-
-        # Récupère les infos sur les colis
-        nb_colis, poid_colis = self.input_info()
-
-        # Calcule la somme total de l'expedition
-        tarif_montant, taxation_montant = self.get_tarif_info(zone_destinataire, code_postal_destinataire, id_destinataire)
-        sum_total = tarif_montant + taxation_montant
+        sum_total = self.montant_tarif + self.montant_taxation
 
         # Présentation des données
         print("--------------------------------------------")
-        print("Expediteur ->\n")
-        print("idClient :", id_expediteur)
-        print("codePostale :", code_postal_expediteur)
-        print("raisonSociale :", raisonSociale_expediteur)
-        print("ville :", ville_expediteur)
+        print("Nombre de colis ->", self.nombre_colis)
+        print("Poids colis ->", self.poids_expedition ,"KG")
         print("--------------------------------------------")
-        print("Destinataire ->\n")
-        print("idClient :", id_destinataire)
-        print("codePostale :", code_postal_destinataire)
-        print("raisonSociale :", raisonSociale_destinataire)
-        print("ville :", ville_destinataire)
-        print("--------------------------------------------")
-        print("Nombre de colis ->", nb_colis)
-        print("Poids colis ->", poid_colis ,"KG")
+        print("Tarifs département ->", self.montant_tarif, "euro")
+        print("Taxation département ->", self.montant_taxation, "euro")
         print("--------------------------------------------")
         print("Le montant total est donc de :", sum_total, "euro")
         print("--------------------------------------------")
-        print("Tarifs département ->", tarif_montant, "euro")
-        print("Taxation département ->", taxation_montant, "euro")
-        print("--------------------------------------------")
 
-# Création d'une instance de la classe
-calcul_transport = Calcul_HT_Transport()
 
-# Appel de la méthode principale pour exécuter le programme
-calcul_transport.main()
+if __name__ == '__main__':
+    HT_Transport = Calcul_HT_Transport()
+    HT_Transport.main()    
